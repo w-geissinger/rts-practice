18dce3c71206d621830d1217ce558752756eb9b8
Wayne Geissinger <w-geissinger@onu.edu>
2022-04-09 22:19:49 -0400

    Added Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, createTheme, ThemeProvider } from '@mui/material';
+
+const theme = createTheme({
+    palette: {
+      primary: {
+        light: '#757ce8',
+        main: '#3f50b5',
+        dark: '#002884',
+        contrastText: '#fff',
+      },
+      secondary: {
+        light: '#ff7961',
+        main: '#f44336',
+        dark: '#ba000d',
+        contrastText: '#000',
+      },
+    },
+  });
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+
+    var X: number = 0;
+
+    var chad: string = 'chad';
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <ThemeProvider theme={theme}>
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        Edit <code>dan@dan-jira.com/App.tsx</code> and save to reload.
+                    </p>
+                    <Button color='primary' variant='contained' onClick={() => { X++ }}>
+                         {`${X} ${chad}`}
+                    </Button>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </ThemeProvider>
+            </header>
+        </div>
+    );
+    
+    
+    
 }
 
 export default App;
